437f484c4afb3a34f8b89aa7ecb8abba92d27b11
Daniel Patru <dpatru@gmail.com>
2020-12-02 14:16:29 -0500

    cleaned up 1

--- 01.hs
+++ 01.hs
@@ -1,20 +1,18 @@
 import Debug.Trace (trace, traceShow, traceShowId)
-import Data.List (sort)
+import Data.List (find, sort, tails)
 
 (&&&) f g x = (f x, g x) -- apply an argument to two functions
 
 tracef f x = traceShow (f x) x
 
--- main = interact $ (++ "\n") . show . (find2 2020 &&& find3 2020) . sort . map read . lines
-main = interact $ (++ "\n") . show . find3 2020 . sort . map read . lines
+main = interact $ (++ "\n") . show . (find2 2020 &&& find3 2020) . sort . map read . lines
+-- main = interact $ (++ "\n") . show . find3 2020 . sort . map read . lines
 
-
-find2 :: Int -> [Int] -> Maybe (Int, Int, Int)
-find2 n (x:xs) = if x' `elem` xs then return (x, x', x * x') else find2 n xs
-  where x' = n - x
+find2 :: Int -> [Int] -> Maybe (Int,Int,Int)
+find2 n (x:xs) = maybe (find2 n xs) (\y -> return (x,y,x*y)) $ find (== n-x) xs
 find2 _ [] = Nothing
 
-find3 n (x:xs) = maybe (find3 n xs)
-                       (\(y,z,_) -> return (x,y,z,x*y*z)) $
-                   find2 (n-x) xs
+find3 :: Int -> [Int] -> Maybe (Int, Int,Int, Int)
+find3 n (x:xs) = maybe (find3 n xs) (\(y,z,_) -> return (x,y,z,x*y*z)) $ find2 (n-x) xs
 find3 _ [] = Nothing
+
