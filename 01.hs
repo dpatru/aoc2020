b5af5fddafe58ef2ed87ee1b52e376259d54deb0
Daniel Patru <dpatru@gmail.com>
2020-12-02 13:27:15 -0500

    day 1

--- 01.hs
+++ 01.hs
@@ -1,5 +1,20 @@
-main = interact $ (++ "\n") . show . find2020 . map read . lines
+import Debug.Trace (trace, traceShow, traceShowId)
+import Data.List (sort)
 
-find2020 (x:xs) = if x' `elem` xs then x * x' else find2020 xs
-  where x' = 2020 - x
-find2020 [] = -1
+(&&&) f g x = (f x, g x) -- apply an argument to two functions
+
+tracef f x = traceShow (f x) x
+
+-- main = interact $ (++ "\n") . show . (find2 2020 &&& find3 2020) . sort . map read . lines
+main = interact $ (++ "\n") . show . find3 2020 . sort . map read . lines
+
+
+find2 :: Int -> [Int] -> Maybe (Int, Int, Int)
+find2 n (x:xs) = if x' `elem` xs then return (x, x', x * x') else find2 n xs
+  where x' = n - x
+find2 _ [] = Nothing
+
+find3 n (x:xs) = maybe (find3 n xs)
+                       (\(y,z,_) -> return (x,y,z,x*y*z)) $
+                   find2 (n-x) xs
+find3 _ [] = Nothing
--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 
 1: 01 01.input.txt
-	./01 <  01.input.txt
+	time ./01 <  01.input.txt
+
+test1: 01 01.test
+	time ./01 < 01.test
 
 01: 01.hs
 	ghc 01.hs
